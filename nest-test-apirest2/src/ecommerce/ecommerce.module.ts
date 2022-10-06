2d04301da62781685f3cd43e93264e7decb45584
Matias Aguilera <aguileramati50@gmail.com>
2022-10-06 12:41:41 -0300

    nest

--- nest-test-apirest2/src/ecommerce/ecommerce.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EcommerceController } from './ecommerce.controller';
-import { EcommerceService } from './ecommerce.service';
-
-@Module({
-  controllers: [EcommerceController],
-  providers: [EcommerceService],
-})
-export class EcommerceModule {}
--- nest-test-apirest2/src/ecommerce/ecommerce.types.ts
+++
@@ -1,6 +0,0 @@
-export class EcommerceDTO {
-  id: number;
-  nombre: string;
-  imagen: string;
-  stock: number;
-}
